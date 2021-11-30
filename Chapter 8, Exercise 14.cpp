afff03478a5ee4c8244a9ed21a481333bc45712f
RMaharaj21 <55695895+RMaharaj21@users.noreply.github.com>
2021-11-29 21:05:29 -0500

    Add files via upload

---
+++ Chapter 8, Exercise 14.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+Can we declare a non-reference function argument const (e.g., void
+f(const int);)? 
+You can declare a non-reference function.
+What might that mean?
+The function deos not need to be referenced.
+Why might we want to do that?
+Why don’t people do that often?
+Try it; write a couple of small programs
+to see what works.
+*******************************************************************************/
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Ryan Maharaj
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
+
